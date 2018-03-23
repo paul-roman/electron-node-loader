601cea01db1013bf3816e56037124ae469eaaeb2
Paul Roman <paul.roman@epitech.eu>
2018-03-23 12:47:49 +0000

    1.0.2

---
+++ README.md
@@ -0,0 +1,16 @@
+## electron-node-loader
+
+This Webpack modules allows you to load a native Node.js C++ addon that can be shipped inside an Electron application.
+
+_Usage:_
+
+```javascript
+loaders: [
+			{
+				test: /\.node?$/,
+				use: 'electron-node-loader'
+			}
+		]
+```
+
+Just add `electron-node-loader&prod=true`for production build.
---
+++ index.js
@@ -0,0 +1,15 @@
+const loaderUtils = require('loader-utils');
+
+module.exports = function() {
+	let outfile = loaderUtils.interpolateName(this, '[path][name].node', {
+		context: this.context,
+	});
+
+	if (loaderUtils.getOptions(this).prod)
+		outfile = 'resources/' + outfile;
+	else
+		outfile = '../' + outfile;
+
+	return "try {global.process.dlopen(module, '" + outfile + "'); } catch(e) {" +
+		"throw new Error('Cannot open ' + " + outfile + " + ': ' + e);}";
+};
